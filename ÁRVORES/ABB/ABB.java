9734ca6e2185ba9e0e4dbe52cc2098a469d4c8bc
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2025-03-10 20:19:36 +0000

    Search binary

--- ÁRVORES/ABB/ABB.java
+++ ÁRVORES/ABB/ABB.java
@@ -20,33 +20,45 @@ public class ABB<T extends Comparable<T>> {
     private void inserir(ABBNode<T> node, T valor){
         //node já é uma cópia do this.root;
         //node não usa o endereço do this.root;
-        ABBNode<T> novo = new ABBNode<T>(valor);
-        int retorno;
-        while(true){
-            retorno = valor.compareTo(node.getInfo());
-            if(retorno >= 0){
-                if(node.getRight() == null){
-                    node.setRight(novo);
-                    return;
+        ABBNode<T> aux = search(this.root, valor);
+        if(aux == null){
+            while(true){
+                ABBNode<T> novo = new ABBNode<T>(valor);
+                int retorno;
+                retorno = valor.compareTo(node.getInfo());
+                if(retorno > 0){
+                    if(node.getRight() == null){
+                        node.setRight(novo);
+                        return;
+                    }else{
+                        node = node.getRight();
+                    }
                 }else{
-                    node = node.getRight();
+                    if(node.getLeft() == null){
+                        node.setLeft(novo);
+                        return;
+                    }else{
+                        node = node.getLeft();
+                    }
                 }
-            
-            
-            }else if(retorno < 0){
-                if(node.getLeft() == null){
-                    node.setLeft(novo);
-                    return;
-                }else{
-                    node = node.getLeft();
-                }
-                
             }
-        } 
-       
+        }else{
+            System.out.println("Ja existe fiao!");
+        }
     }
-        
-        
+    private ABBNode<T> search(ABBNode<T> node, T valor){
+        for(int i = 0; node != null; i++){
+            int retorno = valor.compareTo(node.getInfo());
+            if(retorno == 0){
+                return node;
+            }else if(retorno < 0){
+                node = node.getLeft();
+            }else{
+                node = node.getRight();
+            }
+        }
+        return null;
+    }
 }
 
 
