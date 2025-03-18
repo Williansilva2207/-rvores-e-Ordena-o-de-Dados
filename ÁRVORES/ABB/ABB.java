056655dee9196a0e4b5471a480544eea6ce4c0e2
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2025-03-18 01:33:22 +0000

    att

--- ÁRVORES/ABB/ABB.java
+++ ÁRVORES/ABB/ABB.java
@@ -1,22 +1,22 @@
 package ABB;
 import queue.*;
+import java.util.Stack;
 public class ABB <T extends Comparable<T>> {
     private ABBNode<T> root;
+    private int qtd;
 
-    public boolean isEmpty () {
+    public boolean isEmpty() {
         if (this.root == null) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    public void insert (T valor) {
+    public void insert(T valor) {
         if (this.isEmpty() == true) {
             this.root = new ABBNode<T>(valor);
-        }
-        else {
+        } else {
             inserir(this.root, valor);
         }
     }
@@ -28,16 +28,14 @@ public class ABB <T extends Comparable<T>> {
             if (retorno == 0) {
                 System.out.println("Valor repetido!\n");
                 return;
-            }
-            else if (retorno < 0) {
+            } else if (retorno < 0) {
                 if (node.getLeft() != null) {
                     inserir(node.getLeft(), valor);
                 } else {
                     ABBNode<T> novo = new ABBNode<T>(valor);
                     node.setLeft(novo);
                 }
-            }
-            else {
+            } else {
                 if (node.getRight() != null) {
                     inserir(node.getRight(), valor);
                 } else {
@@ -48,74 +46,72 @@ public class ABB <T extends Comparable<T>> {
         }
     }
 
-    public T find (T value) {
+    public T find(T value) {
         if (this.isEmpty() == true) {
             return null;
-        }
-        else {
-            return findNode (this.root, value);
+        } else {
+            return findNode(this.root, value);
         }
     }
 
-    private T findNode (ABBNode<T> r, T value) {
+    private T findNode(ABBNode<T> r, T value) {
         int result = value.compareTo(r.getInfo());
         if (result == 0) {
             return r.getInfo();
-        }
-        else if (result < 0) {
+        } else if (result < 0) {
             if (r.getLeft() == null) {
                 return null;
+            } else {
+                return findNode(r.getLeft(), value);
             }
-            else {
-                return findNode (r.getLeft(),value);
-            }
-        }
-        else {
+        } else {
             if (r.getRight() == null) {
                 return null;
-             }
-            else {
-                return findNode (r.getRight(),value);
+            } else {
+                return findNode(r.getRight(), value);
             }
         }
     }
-    public searchSmall(){
-        if(this.isEmpty==true){
+
+    public T searchSmall() {
+        if (this.isEmpty() == true) {
             return null;
-        }else{
+        } else {
             return lowestValue(this.root);
         }
     }
-    private T lowestValue(ABBNode <T> r){
-        if(r.getLeft != null){
-            lowestValue(r.getLeft);
-        }else{
+
+    private T lowestValue(ABBNode<T> r) {
+        if (r.getLeft() != null) {
+            lowestValue(r.getLeft());
+        } else {
             return r.getInfo();
         }
     }
 
-    public searchBig(){
-        if(this.isEmpty==true){
+    public T searchBig() {
+        if (this.isEmpty() == true) {
             return null;
-        }else{
+        } else {
             return highestValue(this.root);
         }
     }
-    private T highestValue(ABBNode <T> r){
-        if(r.getRight != null){
-            highestValue(r.getRight);
-        }else{
+
+    public T highestValue(ABBNode<T> r) {
+        if (r.getRight() != null) {
+            highestValue(r.getRight());
+        } else {
             return r.getInfo();
         }
     }
 
-    public void passeioPorNivel () {
-        Queue <ABBNode<T>> fila;
+    public void passeioPorNivel() {
+        Queue<ABBNode<T>> fila;
         ABBNode<T> aux;
         if (this.isEmpty() == false) {
             fila = new Queue();
-            fila.enQueue (this.root);
-            while (fila.isEmpty () == false) {
+            fila.enQueue(this.root);
+            while (fila.isEmpty() == false) {
                 aux = fila.deQueue();
                 if (aux.getLeft() != null) {
                     fila.enQueue(aux.getLeft());
@@ -125,8 +121,44 @@ public class ABB <T extends Comparable<T>> {
                 }
                 System.out.println(aux.getInfo());
             }
-        }else { 
-            System.out.println("Árvore vazia"); 
+        } else {
+            System.out.println("Árvore vazia");
         }
     }
+
+
+
+    public void passeioEmOrdem() {
+        if (this.isEmpty()) {
+            System.out.println("Árvore vazia");
+            return;
+        }
+
+        Stack<ABBNode<T>> stack = new Stack<>();
+        ABBNode<T> atual = this.root;
+
+        while (atual != null || !stack.isEmpty()) {
+
+            while (atual != null) {
+                stack.push(atual);
+                atual = atual.getLeft();
+            }
+
+
+            atual = stack.pop();
+            System.out.println(atual.getInfo());
+
+
+            atual = atual.getRight();
+        }
+    }
+
+    public int contagem(ABBNode<T> r){
+        this.qtd++;
+        if (r != null) {
+            contagem(r.getLeft());
+            contagem(r.getRight());
+        }
+    }
+
 }
